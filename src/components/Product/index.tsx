23b304d2b28b03807428c1c2f734ec0d77f0106e
Carlos199 <luiscarlosortecabral@gmail.com>
2020-09-16 23:20:24 -0400

    Product styles

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 import tshirtImage from '../../assets/tshirt.png';
-import { Container } from "./styles";
+import {
+  Container,
+  Row,
+  Panel,
+  Column,
+  Gallery,
+  Section,
+  Description,
+} from "./styles";
 
 const Product: React.FC = () => {
   return (
@@ -15,11 +23,59 @@ const Product: React.FC = () => {
           <Gallery>
             <img alt="T-Shirt" src={tshirtImage} />
           </Gallery>
+
+          <Info />
         </Column>
-        <Column></Column>
+        <Column>
+
+          {/* <ProductAction /> */}
+          {/* <ProductAction /> */}
+
+          <WarrantySection />
+          <WarrantySection />
+          <WarrantySection />
+        </Column>
       </Panel>
     </Container>
   );
 };
+const WarrantySection = () => (
+  <Section>
+    <h4>Garantia</h4>
+
+    <div>
+      <span>
+        <p className="title">Compra con garantia</p>
+        <p className="description">
+          Recibe el producto con la mejor garantia del año, somo empresa seria
+          entregamos 1 año de garantia por el producto.       </p>
+      </span>
+      <span>
+        <p className="title">Garantia del vendedor</p>
+        <p className="description">Sin garantia</p>
+      </span>
+    </div>
+    <a href="#">Saber mas sobre la garantia</a>
+  </Section>
+)
+const Info = () => (
+  <Description>
+    <h2>Descriccion</h2>
+    <p>
+      Probando describir un producto y ver que tal queda despues en la panatalla.
+    <br />
+      <br />
+    Items Iclusos: <br />
+    - 1x LED <br />
+    - 1x LED <br />
+    - 1x LED <br />
+    - 1x LED <br />
+    - 1x LED <br />
+      <br />
+    Fusque mec fusat afafksaflt a adfaklf ta fsalt faskfkkkt afa ta kfasf aktoafmas f takskfasmfk akm kmfaslkflk aggal kfaf lfkjfakgj akf akagjafm akfkfasl flkflkdslkfg aslfalsf dsfksf
+    kfdflaf dkflñakfadkfpakflñadkflñakfladkfkalñ fkl  kfdñlfkds dfklk
+    </p>
+  </Description>
+)
 
 export default Product;
